f1a35f4c8cb746c50bd87dab6e36a290177aadce
Marko Kostovski <marko.kostovski@outlook.com>
2017-04-24 15:04:32 +0200

    All version updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,35 @@
 var webpack = require('webpack');
-var jsLoaders = ['babel?presets[]=react,presets[]=es2015,plugins[]=add-module-exports'];
+var jsLoaders = ['babel-loader?presets[]=react,presets[]=es2015,plugins[]=add-module-exports,plugins[]=transform-runtime,presets[]=stage-0'];
 
 module.exports = {
-    entry: [
-        'webpack/hot/only-dev-server',
-        './index.jsx'
-    ],
-    output: {
-        path: __dirname + '/build',
-        filename: "bundle.js"
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.jsx$/,
-                exclude: /node_modules/,
-                loaders: ['react-hot'].concat(jsLoaders)
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel?presets[]=react,presets[]=es2015,plugins[]=add-module-exports'
-            },
-            {
-                test: /\.css$/,
-                loader: "style!css"
-            }
-        ]
-    },
-    plugins: [
-        new webpack.NoErrorsPlugin()
-    ]
+	entry: [
+		'webpack/hot/only-dev-server',
+		'./index.jsx'
+	],
+	output: {
+		path: __dirname + '/build',
+		filename: "bundle.js"
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.jsx$/,
+				exclude: /node_modules/,
+				loaders: ['react-hot-loader'].concat(jsLoaders)
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: jsLoaders
+			},
+			{
+				test: /\.css$/,
+				loader: "style!css"
+			}
+		]
+	},
+	plugins: [
+		new webpack.NoEmitOnErrorsPlugin()
+	]
 
 };
